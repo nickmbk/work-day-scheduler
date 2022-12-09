e2307fdbc9df0cc47965d25db9ebe75502c087f3
Nick <72912456+nickmbk@users.noreply.github.com>
2022-12-09 20:28:13 +0000

    add colors based on time to the textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,16 @@ var getDay = moment().format("dddd MMMM, Do");
 $("#currentDay").append(getDay);
 
 // hour colors
-var getTime = moment()
+var hourNow = moment().hour();
+
+for (var i = 9; i < 18; i++) {
+    $("textarea").each(function() {
+        if (i < hourNow) {
+            $("textarea").addClass("past");
+        } else if (i === hourNow) {
+            $("textarea").addClass("present");
+        } else if (i > hourNow) {
+            $("textarea").addClass("future");
+        }
+    })
+}
